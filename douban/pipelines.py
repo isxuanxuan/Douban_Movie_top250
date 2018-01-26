0baa81d7ecceecd656d129c14ba9db6343b267d7
monystar <zxxmanagement@gmail.com>
2018-01-26 12:58:58 +0800

    删除代码多余部分

--- douban/pipelines.py
+++ douban/pipelines.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import csv
-
 class DoubanPipeline(object):
     line = ['movieRank','movieName','moviePic','starNum','quoteNum','movieDes']
     def open_spider(self,spider):
